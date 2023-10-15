075cf72b7e77bf3b00a9846d2ba5f3733fbeddef
Umid2720 <umidbekruzmetov309@gmail.com>
2023-10-15 15:51:30 +0500

    has been added default files, packages and deleted unused components

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import top from './components/navbar/top.vue'
+import IconPhone from './assets/icons/IconPhone.vue'
+import IconHeart from './assets/icons/IconHeart.vue'
+import IconShopping from './assets/icons/IconShopping.vue'
+</script>
+
+<template>
+  <top />
+  <header>
+    <nav class="wrapper mx-auto flex items-center py-5 main-color">
+      <a href="/">
+        <img src="logo.png" alt="logo" />
+      </a>
+      <ul class="flex items-center ml-[101px] gap-x-8 text-xl">
+        <li>
+          <router-link to="/">Главная</router-link>
+        </li>
+        <li>Каталог</li>
+        <li>
+          <router-link to="/">Оптовая продажа</router-link>
+        </li>
+        <li>
+          <router-link to="/">О нас</router-link>
+        </li>
+      </ul>
+      <ul class="flex items-center ml-auto">
+        <li class="flex items-end">
+          <IconPhone class="w-5 h-5 text-blue-500" />
+          <a href="tel:+79665588499" class="text-lg">+7 (966) 55 88 499</a>
+        </li>
+        <li class="ml-7 mr-6">
+          <IconHeart class="w-6 h-6" />
+        </li>
+        <li>
+          <IconShopping class="w-6 h-6" />
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/components/navbar/top.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+  <section class="bg-[#454F5B] h-11 shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
+    <div class="flex wrapper py-[9px] text-center justify-between mx-auto">
+      <p class="text-white font-medium text-[16px] leading-[26px] tracking-[1.92px] mx-auto">Скидка 10% по промокоду “ZAMOK” на все заказы до 10.09</p>
+      <a href="" class="text-white font-medium leading-[26px] text-[16px] underline tracking-[1.92px]">Обратный звонок</a>
+    </div>
+  </section>
+</template>
