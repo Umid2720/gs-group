9f972bf8447f3be0aee821e2d99fb38f6eb4321a
Umid2720 <umidbekruzmetov309@gmail.com>
2023-10-17 23:45:09 +0500

    fit (top.vue): has been added responsive top into top.vue

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,31 @@ import IconShopping from './assets/icons/IconShopping.vue'
 <template>
   <top />
   <header>
-    <nav class="wrapper mx-auto flex items-center py-5 main-color">
-      <a href="/">
+    <nav class="wrapper mx-auto flex items-center py-5 relative px-4">
+      <button class="relative group cursor-pointer h-5 w-6 inline md:hidden">
+        <span
+          class="w-6 h-0.5 rounded bg-black duration-300 left-0 inline-block group-hover:rotate-45 group-hover:top-2 absolute top-0"
+        ></span>
+        <span
+          class="w-6 h-0.5 rounded bg-black duration-300 left-0 inline-block absolute group-hover:-rotate-45 group-hover:top-2"
+        ></span>
+        <span
+          class="w-6 h-0.5 rounded bg-black duration-1000 left-0 inline-block absolute bottom-0 group-hover:hidden"
+        ></span>
+      </button>
+
+      <a
+        href="#"
+        class="absolute right-4 -top-8 lg:-top-9 text-[12px] lg:text-[16px] underline font-medium tracking-[1.92px] lg:tracking-[2%] hidden md:inline text-white"
+        >Обратный звонок</a
+      >
+      <a href="/" class="hidden md:inline">
         <img src="logo.png" alt="logo" />
       </a>
-      <ul class="flex items-center ml-[101px] gap-x-8 text-xl">
+
+      <ul class="hidden md:flex items-center ml-16 lg:ml-24 gap-x-8 text-sm lg:text-xl main-color">
         <li>
-          <router-link to="/">Главная</router-link>
+          <router-link to="/" class="">Главная</router-link>
         </li>
         <li>Каталог</li>
         <li>
@@ -25,9 +43,9 @@ import IconShopping from './assets/icons/IconShopping.vue'
         </li>
       </ul>
       <ul class="flex items-center ml-auto">
-        <li class="flex items-end">
+        <li class="items-end md:flex hidden">
           <IconPhone class="w-5 h-5 text-blue-500" />
-          <a href="tel:+79665588499" class="text-lg">+7 (966) 55 88 499</a>
+          <a href="tel:+79665588499" class="text-sm lg:text-lg">+7 (966) 55 88 499</a>
         </li>
         <li class="ml-7 mr-6">
           <IconHeart class="w-6 h-6" />
--- src/components/navbar/top.vue
+++ src/components/navbar/top.vue
@@ -1,9 +1,10 @@
 <script setup></script>
 <template>
-  <section
-    class="tracking-[1.92px] font-medium text-white bg-[#454F5B] md:h-11 h-9 shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] md:py-[9px] py-3 px-6 text-[12px] xl:text-[16px] md:leading-[26px] flex flex-col justify-center leading-[14px]"
-  >
-    <p class="font-medium text-center">Скидка 10% по промокоду “ZAMOK” на все заказы до 10.09</p>
-    <a href="#" class="absolute right-[100px] top-2 underline hidden lg:inline">Обратный звонок</a>
+  <section class="">
+    <p
+      class="bg-[#454F5B] md:h-11 h-9 shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] lg:py-[9px] py-3 flex flex-col justify-center px-6 font-medium text-center md:tracking-[1.92px] tracking-[2%] text-white text-[12px] lg:text-[16px] lg:leading-[26px] leading-[14px]"
+    >
+      Скидка 10% по промокоду “ZAMOK” на все заказы до 10.09
+    </p>
   </section>
 </template>
